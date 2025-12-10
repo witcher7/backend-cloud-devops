a2b912465942612ec2800b10dc6c92b513a200db
Rish <gulati.r7121@gmail.com>
2025-12-10 15:36:35 +0530

    Update read.py

--- python-base/workingfiles/read.py
+++ python-base/workingfiles/read.py
@@ -2,3 +2,10 @@ file = open('jokes.txt', "r")
 content = file.read()
 print(content)
 # read method reads all the lines
+file.close()
+# it is always important to close the file to release system resources 
+# readlines the file line by line 
+with open('jokes.txt', "r") as file:
+ lines = file.readlines()
+ for line in lines:
+  print(line.strip())
