1fd1cb3324c6d09863c38ccfec85e0a3e2d45afb
Rish <gulati.r7121@gmail.com>
2025-12-11 14:31:22 +0530

    Update paths.py

--- python-base/workingfiles/paths.py
+++ python-base/workingfiles/paths.py
@@ -1,8 +1,29 @@
+# from pathlib import Path
+# p = Path('.') # current working directory
+# print(p,'is the current working directory ')
+# home = p.home()
+# print(home)
+# doc_path = home / 'documents'
+# print(doc_path)
+# print(doc_path.parent) # just above the parent of existing folder 
+
+# ### if the file does not exists
+
+# from pathlib import Path 
+# crazy_path = Path.home() / 'I' / 'dont' / 'exist.csv'
+# print(crazy_path)
+
+# if crazy_path.exists():
+#  with open(crazy_path, "r") as f:
+#      print(f.read())
+# else: 
+#    print("The file does not exist")
+
+## The iterdir method generates an iterator for files and folders within a directory, facilitating easy traversal
+# of these items 
+
+## this allows us to iterate over each item( file or folder) one at a time 
 from pathlib import Path
-p = Path('.') # current working directory
-print(p,'is the current working directory ')
-home = p.home()
-print(home)
-doc_path = home / 'documents'
-print(doc_path)
-print(doc_path.parent) # just above the parent of existing folder 
+path = Path.home() 
+for item in path.iterdir():
+   print(item) 
