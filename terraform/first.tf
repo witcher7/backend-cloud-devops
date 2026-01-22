c85a3b1c29c42d72387daf72e656d747af4e235e
Rish <gulati.r7121@gmail.com>
2026-01-22 17:05:47 +0530

    Add AWS providers and VPC resource to Terraform config
    Introduced two AWS provider blocks for us-east-1 and us-west-1 regions, including credentials and profile configuration. Added an aws_vpc resource using the us-west-1 provider with a specified CIDR block and tag.

--- terraform/first.tf
+++ terraform/first.tf
@@ -12,8 +12,33 @@ terraform {
   }
 }
 
+## provider block 
+provider "aws" {
+  region = "us-east-1"
+  access_key = "AKIAIOSFODNN7EXAMPLE"
+  secret_key = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
+  shared_credentials_files = ["~/.aws/credentials"]
+  profile = "default"
+}
+
 ## Whenever we run terraform init, it will download the required providers and create a lock file
 ## if we change the version of providers, it will update the lock file but we need to run terraform init again
 ## terraform init --upgrade will upgrade the providers to the latest version  
 ## terraform init -reconfigure will reconfigure the providers
 ## terraform init -force-copy will force copy the providers
+
+
+## We can have multiple providers 
+provider "aws" {
+  region = "us-west-1"
+  profile = "default"
+  alias = "us-west-1" # we are giving alias to provider to differentiate between providers
+}
+
+resource "aws_vpc" "west-vpc" {
+  cidr_block = "10.0.0.0/16"
+  provider = aws.us-west-1
+  tags = {
+     "Name" = "us-west-1"
+  }
+}
